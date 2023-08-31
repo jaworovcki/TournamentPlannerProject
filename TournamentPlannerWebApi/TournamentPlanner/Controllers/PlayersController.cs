a9185cfa0db5c08475d29c37af112aad9023276b
Andrii <jaworovcki@gmail.com>
2023-08-31 20:44:15 +0300

    Create PlayersContoller and inject the dbContext

---
+++ TournamentPlannerWebApi/TournamentPlanner/Controllers/PlayersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Data;
+
+namespace TournamentPlanner.Controllers
+{
+	[ApiController]
+	[Route("api")]
+	public class PlayersController : ControllerBase
+	{
+		private readonly TournamentPlannerDbContext _dbContext;
+
+		public PlayersController(TournamentPlannerDbContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+
+    }
+}
