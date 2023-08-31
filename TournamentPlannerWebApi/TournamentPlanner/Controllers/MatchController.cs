8503c34905d874a8fa0aca9a30f34e52bb987491
Andrii <jaworovcki@gmail.com>
2023-09-01 00:11:08 +0300

    Create MatchController which can get all incompleted matches and generate the next round matches if this round is over

---
+++ TournamentPlannerWebApi/TournamentPlanner/Controllers/MatchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentPlanner.Data;
+
+namespace TournamentPlanner.Controllers
+{
+	[ApiController]
+	[Route("/api/matches")]
+	public class MatchController : ControllerBase
+	{
+		private readonly TournamentPlannerDbContext _dbContext;
+
+		public MatchController(TournamentPlannerDbContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+
+		[HttpGet("open")]
+		public async Task<IList<Match>> GetAllIncompletMatchesAsync()
+			=> await _dbContext.GetIncompleteMatches();
+
+		[HttpPost("generate")]
+		public async Task GenerateNextRoundMatchesAsync()
+			=> await _dbContext.GenerateMatchesForNextRound();
+    }
+}
